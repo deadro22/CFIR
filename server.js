7945693dddce255c9276b8da356972e7cc4e5b99
deadro22 <44213280+deadro22@users.noreply.github.com>
2019-06-08 23:11:13 +0100

    bigHc

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "pages/javascript"));
+app.use(express.static(__dirname + "/pages"));
+app.use(express.static(__dirname + "/pages/styles"));
+app.use(express.static(__dirname + "pages/images"));
+
+
+app.get("/",function(req,res){
+  res.render(__dirname + "/pages/home.ejs");
+});
+app.get("/home",function(req,res){
+  res.render(__dirname + "/pages/home.ejs");
+});
+app.get("/activities/Training",function(req,res){
+  res.render(__dirname + "/pages/Training.ejs");
+});
+app.get("/activities/Lectures",function(req,res){
+  res.render(__dirname + "/pages/Lectures.ejs");
+});
+app.get("/activities/Trips",function(req,res){
+  res.render(__dirname + "/pages/trips.ejs");
+});
+app.get("/banking",function(req,res){
+  res.render(__dirname + "/pages/progress.ejs");
+});
+app.get("/docs",function(req,res){
+  res.render(__dirname + "/pages/progress.ejs");
+});
+app.get("/contact",function(req,res){
+  res.render(__dirname + "/pages/progress.ejs");
+});
+app.get("*",function(req,res){
+  res.render(__dirname + "/pages/error.ejs");
+});
+app.listen(80);
+
