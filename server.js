a8090d58ac1546f3cd294f4b397f3b97734396d1
deadro22 <44213280+deadro22@users.noreply.github.com>
2019-08-12 17:56:58 +0100

    enter

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 
 app.use(express.static(__dirname + "/javascript"));
 app.use(express.static(__dirname + "/pages"));
@@ -91,7 +90,7 @@ app.get(["/", "/home"], function(req, res) {
     res.render(__dirname + "/pages/home.ejs", { vids });
   });
 });
-app.get("/activities/:dt_type", checkAuth, function(req, res) {
+app.get("/activities/:dt_type", function(req, res) {
   webdats.find({ dt_type: req.params.dt_type }).then(function(wb_data) {
     if (wb_data != "") {
       var re_type =
@@ -111,7 +110,7 @@ app.get("/docs", function(req, res) {
   res.render(__dirname + "/pages/progress.ejs");
 });
 app.get("/forum", function(req, res) {
-  res.render(__dirname + "/pages/forum.ejs");
+  res.render(__dirname + "/pages/progress.ejs");
 });
 app.get("/www.", function(req, res) {
   res.redirect("/home");
@@ -130,100 +129,7 @@ app.get("/admin/dashboard/post/:code", function(req, res) {
 app.get("*", function(req, res) {
   res.status(404).render(__dirname + "/pages/error.ejs");
 });
-app.post("/auth/register", function(req, res) {
-  var u_data = {
-    email: req.body.rEmail,
-    name: req.body.rUsername,
-    password: req.body.rPassword,
-    confirmPass: req.body.rCPassword
-  };
-  if (
-    !u_data.email ||
-    !u_data.name ||
-    !u_data.password ||
-    !u_data.confirmPass
-  ) {
-    req.flash("err_msg", "All fields are required");
-    res.redirect("back");
-  } else if (u_data.password.length < 8) {
-    req.flash("err_msg", "Passwords should be at least 8 characters long");
-    res.redirect("back");
-  } else if (u_data.password != u_data.confirmPass) {
-    req.flash("err_msg", "Passwords does not match");
-    res.redirect("back");
-  } else {
-    users
-      .findOne({ uname: u_data.name, email: u_data.email })
-      .then(function(user) {
-        if (user) {
-          req.flash("err_msg", "Account already exists");
-          res.redirect("back");
-        } else {
-          const n_user = new users({
-            email: u_data.email,
-            uname: u_data.name,
-            password: u_data.password
-          });
-          bcrypt.genSalt(10, function(error, salt) {
-            bcrypt.hash(n_user.password, salt, function(err, hash) {
-              if (err) {
-                req.flash(
-                  "err_msg",
-                  "There was an error creating your account"
-                );
-                res.redirect("back");
-              } else {
-                n_user.password = hash;
-                n_user.save().then(function(s_us) {
-                  res.redirect("/home");
-                });
-              }
-            });
-          });
-        }
-      });
-  }
-});
-app.post("/auth/login", function(req, res) {
-  if (!req.body.lEmail || !req.body.lPassword) {
-    req.flash("lerr_msg", "All fields are required");
-    res.redirect("back");
-    res.end();
-  } else {
-    users.findOne({ email: req.body.lEmail }).then(function(user) {
-      if (!user) {
-        req.flash("lerr_msg", "Account does not exist");
-        res.redirect("back");
-      } else {
-        bcrypt.compare(req.body.lPassword, user.password, function(
-          err,
-          isMatch
-        ) {
-          if (err) {
-            res.redirect("back");
-          } else if (isMatch) {
-            jwt.sign(
-              { email: user.email, userId: user._id },
-              process.env.JWT_KEY,
-              {
-                expiresIn: "1h"
-              },
-              (err, token) => {
-                res.status(200).json({
-                  msg: "auth success",
-                  token: token
-                });
-              }
-            );
-          } else {
-            req.flash("lerr_msg", "Password incorrect");
-            res.redirect("back");
-          }
-        });
-      }
-    });
-  }
-});
+
 app.post("/content/add", function(req, res) {
   if (
     !req.body.title ||
